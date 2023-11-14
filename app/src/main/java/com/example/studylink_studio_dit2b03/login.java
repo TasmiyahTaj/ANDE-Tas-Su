9ee3d2fb39a64c32c29eb421fce095f7af15a4cd
Tasmiyah <tasmiyahtaj26@gmail.com>
2023-11-14 10:38:57 +0800

    login

---
+++ app/src/main/java/com/example/studylink_studio_dit2b03/login.java
@@ -0,0 +1,13 @@
+package com.example.studylink_studio_dit2b03;
+
+import android.os.Bundle;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+public class login extends AppCompatActivity {
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.login);
+    }
+}
