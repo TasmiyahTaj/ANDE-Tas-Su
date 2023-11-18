92bbd9d6c251a4f402cfd69100bb574137f5286d
suthiri131 <stk15915@gmail.com>
2023-11-19 01:29:24 +0800

    home page finally done

--- app/src/main/java/com/example/studylink_studio_dit2b03/SignUpPrompt.java
+++ app/src/main/java/com/example/studylink_studio_dit2b03/SignUpPrompt.java
@@ -8,27 +8,27 @@ import android.widget.Toast;
 
 import androidx.appcompat.app.AppCompatActivity;
 
-//public class SignUpPrompt extends AppCompatActivity implements View.OnClickListener{
-//    int token = 0;
-//    private String dUsername;
-//    TextView rUsername;
-//    String msg = "From SignUpPrompt Class : ";
-//
-//
-//    @Override
-//    protected void onCreate(Bundle savedInstanceState) {
-//        super.onCreate(savedInstanceState);
-//        setContentView(R.layout.signup);
-//
-//        // Example: Connecting verifyToken method to a button
-//
-//        rUsername=(TextView) findViewById(R.id.txtUsername);
-//        Bundle getData = getIntent().getExtras();
-//        if(getData !=null){
-//            dUsername=getData.getString("rUsername");
-//            rUsername.setText(dUsername);
-//        }else{
-//            Toast.makeText(getApplicationContext(),"Failed to get Data",Toast.LENGTH_LONG).show();
-//        }
-//    }
-//}
+public class SignUpPrompt extends AppCompatActivity implements View.OnClickListener{
+    int token = 0;
+    private String dUsername;
+    TextView rUsername;
+    String msg = "From SignUpPrompt Class : ";
+
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.signup);
+
+        // Example: Connecting verifyToken method to a button
+
+        rUsername=(TextView) findViewById(R.id.txtUsername);
+        Bundle getData = getIntent().getExtras();
+        if(getData !=null){
+            dUsername=getData.getString("rUsername");
+            rUsername.setText(dUsername);
+        }else{
+            Toast.makeText(getApplicationContext(),"Failed to get Data",Toast.LENGTH_LONG).show();
+        }
+    }
+}
