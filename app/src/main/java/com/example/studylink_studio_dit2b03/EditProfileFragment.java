a6419f9919881363d24f909b145c932110eb45fc
Tasmiyah <tasmiyahtaj26@gmail.com>
2024-02-09 18:32:26 +0800

    git

--- app/src/main/java/com/example/studylink_studio_dit2b03/EditProfileFragment.java
+++ app/src/main/java/com/example/studylink_studio_dit2b03/EditProfileFragment.java
@@ -1,6 +1,8 @@
 package com.example.studylink_studio_dit2b03;
 
+import android.app.Activity;
 import android.content.Context;
+import android.content.Intent;
 import android.content.SharedPreferences;
 import android.net.Uri;
 import android.os.Bundle;
@@ -13,15 +15,26 @@ import android.widget.Button;
 import android.widget.EditText;
 import android.widget.ImageView;
 import android.widget.Spinner;
+import android.widget.Toast;
 
+import androidx.annotation.NonNull;
 import androidx.fragment.app.Fragment;
 
+import com.google.android.gms.tasks.OnFailureListener;
+import com.google.android.gms.tasks.OnSuccessListener;
 import com.google.common.reflect.TypeToken;
+import com.google.firebase.firestore.DocumentReference;
+import com.google.firebase.firestore.FirebaseFirestore;
+import com.google.firebase.storage.FirebaseStorage;
+import com.google.firebase.storage.StorageReference;
+import com.google.firebase.storage.UploadTask;
 import com.google.gson.Gson;
 import com.squareup.picasso.Picasso;
 
 import java.lang.reflect.Type;
 import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.Map;
 
 public class EditProfileFragment extends Fragment {
 
@@ -37,8 +50,9 @@ public class EditProfileFragment extends Fragment {
     private EditText yearOfExperienceEditText;
     private EditText accountNumberEditText;
     private Button changePasswordButton;
-
-
+    private FirebaseFirestore db = FirebaseFirestore.getInstance();
+    private static final int GALLERY_REQUEST_CODE = 1;
+    private Uri selectedImageUri;
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
         View rootView = inflater.inflate(R.layout.edit_profile, container, false);
@@ -101,17 +115,197 @@ public class EditProfileFragment extends Fragment {
             }
         });
 
+        Button saveBtn = rootView.findViewById(R.id.button_save); // Assuming the ID of your save button is button_save_profile
+
+        saveBtn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                saveProfileChanges();
+            }
+        });
         return rootView;
     }
 
+    private void updateUserInformation(String newUsername) {
+        // Get the reference to the user's document in Firestore
+        DocumentReference userDocRef = db.collection("users").document(userInstance.getUserid());
 
-    private void changeProfilePicture() {
-        // Implement profile picture change functionality
+        // Update the "username" field with the new username
+        userDocRef.update("username", newUsername)
+                .addOnSuccessListener(new OnSuccessListener<Void>() {
+                    @Override
+                    public void onSuccess(Void aVoid) {
+                        Log.d("EditProfileFragment", "User's username updated successfully");
+                        // Optionally, perform any action after the update is successful
+                    }
+                })
+                .addOnFailureListener(new OnFailureListener() {
+                    @Override
+                    public void onFailure(@NonNull Exception e) {
+                        Log.e("EditProfileFragment", "Error updating user's username", e);
+                        // Optionally, handle the error here
+                    }
+                });
     }
 
+    private void updateStudentInformation(String newUsername, String newInstitution, String newCourse) {
+        // Get the reference to the student's document in Firestore
+        DocumentReference studentDocRef = db.collection("Student").document(userInstance.getUserid());
+
+        // Create a map with the updated fields
+        Map<String, Object> updates = new HashMap<>();
+        updates.put("username", newUsername);
+        updates.put("institutionid", newInstitution);
+        updates.put("courseid", newCourse);
+
+        // Update the student's document with the new data
+        studentDocRef.update(updates)
+                .addOnSuccessListener(new OnSuccessListener<Void>() {
+                    @Override
+                    public void onSuccess(Void aVoid) {
+                        Log.d("EditProfileFragment", "Student information updated successfully");
+                        // Optionally, perform any action after the update is successful
+                    }
+                })
+                .addOnFailureListener(new OnFailureListener() {
+                    @Override
+                    public void onFailure(@NonNull Exception e) {
+                        Log.e("EditProfileFragment", "Error updating student information", e);
+                        // Optionally, handle the error here
+                    }
+                });
+    }
+    private void changeProfilePicture() {
+        // Implement profile picture change functionality
+        // Start an intent to choose an image from the device's gallery
+        Intent intent = new Intent(Intent.ACTION_PICK);
+        intent.setType("image/*");
+        startActivityForResult(intent, GALLERY_REQUEST_CODE);
+    }
+
+    @Override
+    public void onActivityResult(int requestCode, int resultCode, Intent data) {
+        super.onActivityResult(requestCode, resultCode, data);
+        if (requestCode == GALLERY_REQUEST_CODE && resultCode == Activity.RESULT_OK && data != null && data.getData() != null) {
+            // Get the Uri of the selected image
+           selectedImageUri = data.getData();
+
+            // Upload the image to Firebase Storage
+            profilePictureImageView.setImageURI(selectedImageUri);
+        }else {
+            // No image selected, proceed with saving profile changes
+            saveProfileChanges();
+        }
+    }
+
+    private void uploadProfilePicture(Uri imageUri) {
+        // Create a reference to the profile picture file in Firebase Storage
+        StorageReference profilePicRef = FirebaseStorage.getInstance().getReference()
+                .child("profile_pictures")
+                .child(userInstance.getUserid() + ".jpg");
+
+        // Upload the image to Firebase Storage
+        profilePicRef.putFile(imageUri)
+                .addOnSuccessListener(new OnSuccessListener<UploadTask.TaskSnapshot>() {
+                    @Override
+                    public void onSuccess(UploadTask.TaskSnapshot taskSnapshot) {
+                        // Image uploaded successfully, get the download URL
+                        profilePicRef.getDownloadUrl().addOnSuccessListener(new OnSuccessListener<Uri>() {
+                            @Override
+                            public void onSuccess(Uri uri) {
+                                // Update the profile picture URL in the database
+                                updateUserProfilePicture(uri.toString());
+                            }
+                        });
+                    }
+                })
+                .addOnFailureListener(new OnFailureListener() {
+                    @Override
+                    public void onFailure(@NonNull Exception e) {
+                        // Handle unsuccessful upload
+                        Log.e("EditProfileFragment", "Failed to upload profile picture", e);
+                    }
+                });
+    }
+
+    private void updateUserProfilePicture(String profilePicUrl) {
+        // Get the reference to the user's document in Firestore
+        DocumentReference userDocRef = db.collection("users").document(userInstance.getUserid());
+
+        // Update the "profilePicUrl" field with the new profile picture URL
+        userDocRef.update("profilePicUrl", profilePicUrl)
+                .addOnSuccessListener(new OnSuccessListener<Void>() {
+                    @Override
+                    public void onSuccess(Void aVoid) {
+                        Log.d("EditProfileFragment", "User's profile picture updated successfully");
+                        // Optionally, perform any action after the update is successful
+                    }
+                })
+                .addOnFailureListener(new OnFailureListener() {
+                    @Override
+                    public void onFailure(@NonNull Exception e) {
+                        Log.e("EditProfileFragment", "Error updating user's profile picture", e);
+                        // Optionally, handle the error here
+                    }
+                });
+    }
     private void changePassword() {
         // Implement change password functionality
     }
+    private void saveProfileChanges() {
+        // Retrieve updated information
+        String newUsername = usernameEditText.getText().toString();
+        String newQualification = qualificationSpinner.getSelectedItem().toString();
+        String newSpecialization = specializationSpinner.getSelectedItem().toString();
+
+        // Update user's information in the Users collection
+        updateUserInformation(newUsername);
+
+        // If the user is a student, update their information in the Student collection
+        if (roleId == 1) { // Student
+            String newInstitution = institutionSpinner.getSelectedItem().toString();
+            String newCourse = courseSpinner.getSelectedItem().toString();
+            updateStudentInformation(newUsername, newInstitution, newCourse);
+            Toast.makeText(getActivity(), "Profile changes saved successfully", Toast.LENGTH_SHORT).show();
+        }
+        // If the user is a tutor, update their information in the Tutor collection
+        else if (roleId == 2) { // Tutor
+            updateTutorInformation(newUsername, newQualification, newSpecialization);
+            Toast.makeText(getActivity(), "Profile changes saved successfully", Toast.LENGTH_SHORT).show();
+        }
+        if (selectedImageUri != null) {
+            uploadProfilePicture(selectedImageUri);
+        }
+        // Optionally, you can show a toast or perform any other action to indicate successful update
+    }
+    private void updateTutorInformation(String newUsername, String newQualification, String newSpecialization) {
+        // Get the reference to the tutor's document in Firestore
+        DocumentReference tutorDocRef = db.collection("Tutor").document(userInstance.getUserid());
+
+        // Create a map with the updated fields
+        Map<String, Object> updates = new HashMap<>();
+        updates.put("username", newUsername);
+        updates.put("qualification", newQualification);
+        updates.put("specialised", newSpecialization);
+
+        // Update the tutor's document with the new data
+        tutorDocRef.update(updates)
+                .addOnSuccessListener(new OnSuccessListener<Void>() {
+                    @Override
+                    public void onSuccess(Void aVoid) {
+                        Log.d("EditProfileFragment", "Tutor information updated successfully");
+                        // Optionally, perform any action after the update is successful
+                    }
+                })
+                .addOnFailureListener(new OnFailureListener() {
+                    @Override
+                    public void onFailure(@NonNull Exception e) {
+                        Log.e("EditProfileFragment", "Error updating tutor information", e);
+                        // Optionally, handle the error here
+                    }
+                });
+    }
+
     private void populateSpinners() {
         SharedPreferences sharedPreferences = getActivity().getSharedPreferences("user_preferences", Context.MODE_PRIVATE);
 
@@ -159,55 +353,55 @@ public class EditProfileFragment extends Fragment {
             Log.e("EditProfileFragment", "Specialisations list is null");
         }
 //
-//        // Get the Student object
-//        Student student = userInstance.getStudent();
-//        if (student != null) {
-//            String institution = student.getInstitutionid();
-//            String course = student.getCourseid();
-//
-//            // Set default value for institution spinner
-//            if (institution != null && institutions != null) {
-//                int index = institutions.indexOf(institution);
-//                if (index != -1) {
-//                    institutionSpinner.setSelection(index);
-//                }
-//            }
-//
-//            // Set default value for course spinner
-//            if (course != null && courses != null) {
-//                int index = courses.indexOf(course);
-//                if (index != -1) {
-//                    courseSpinner.setSelection(index);
-//                }
-//            }
-//        }else{
-//            Log.d("Edit profile","not a student");
-//        }
-//
-//        // Get the Tutor object
-//        Tutor tutor = userInstance.getTutor();
-//        if (tutor != null) {
-//            String qualification = tutor.getQualification();
-//            String specialisation = tutor.getSpecialised();
-//
-//            // Set default value for qualification spinner
-//            if (qualification != null && qualifications != null) {
-//                int index = qualifications.indexOf(qualification);
-//                if (index != -1) {
-//                    qualificationSpinner.setSelection(index);
-//                }
-//            }
-//
-//            // Set default value for specialization spinner
-//            if (specialisation != null && specialisations != null) {
-//                int index = specialisations.indexOf(specialisation);
-//                if (index != -1) {
-//                    specializationSpinner.setSelection(index);
-//                }
-//            }
-//        }else{
-//            Log.d("Edit profile","not a teacher");
-//        }
+        // Get the Student object
+        Student student = userInstance.getStudent();
+        if (student != null) {
+            String institution = student.getInstitutionid();
+            String course = student.getCourseid();
+
+            // Set default value for institution spinner
+            if (institution != null && institutions != null) {
+                int index = institutions.indexOf(institution);
+                if (index != -1) {
+                    institutionSpinner.setSelection(index);
+                }
+            }
+
+            // Set default value for course spinner
+            if (course != null && courses != null) {
+                int index = courses.indexOf(course);
+                if (index != -1) {
+                    courseSpinner.setSelection(index);
+                }
+            }
+        }else{
+            Log.d("Edit profile","not a student");
+        }
+
+        // Get the Tutor object
+        Tutor tutor = userInstance.getTutor();
+        if (tutor != null) {
+            String qualification = tutor.getQualification();
+            String specialisation = tutor.getSpecialised();
+
+            // Set default value for qualification spinner
+            if (qualification != null && qualifications != null) {
+                int index = qualifications.indexOf(qualification);
+                if (index != -1) {
+                    qualificationSpinner.setSelection(index);
+                }
+            }
+
+            // Set default value for specialization spinner
+            if (specialisation != null && specialisations != null) {
+                int index = specialisations.indexOf(specialisation);
+                if (index != -1) {
+                    specializationSpinner.setSelection(index);
+                }
+            }
+        }else{
+            Log.d("Edit profile","not a teacher");
+        }
     }
 
 }
